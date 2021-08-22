8c10893561feefc188a9c7cb2f763e64450c573e
jaikishan15 <61348468+jaikishan15@users.noreply.github.com>
2021-08-22 13:12:18 +0530

    Update cgi-bin.py

--- cgi-bin.py
+++ cgi-bin.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+print("content-type:text/html")
+print()
 
+import cgi
+import subprocess
+
+data=cgi.FieldStorage()
+cmd=data.getvalue("x")
+a=subprocess.getoutput("sudo " +cmd)
+print(a)
